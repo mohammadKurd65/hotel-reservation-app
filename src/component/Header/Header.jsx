ba2a218f7a1ad22483707fad0aeee60f29eed42a
mohamadkurd <hsemohamad385@gmail.com>
2024-06-03 18:30:27 +0330

    add component GuestOptionList

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react"
-import { HiCalendar, HiSearch } from "react-icons/hi"
+import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi"
 import { MdLocationOn } from "react-icons/md"
 
 
 function Header() {
     const[destination, setDestination] = useState("")
+    const [openOptions, setOpenOptions] = useState(false)
+    const [options, setOptions] = useState({
+        adult: 1,
+        children: 0,
+        room: 1
+    })
 return (
     <div className="header">
+        <h2>Home</h2>
         <div className="headerSearch">
             <div className="headerSearchItem">
                 <MdLocationOn className="headerIcon locationIcon"/>
@@ -24,9 +31,11 @@ return (
                 <span className="seperator"></span>
             </div>
             <div className="headerSearchItem">
-                <div id="optionDropDown">
+                <div id="optionDropDown" 
+                onClick={()=> setOpenOptions(!openOptions)}>
                     1 adult . 2 children . 1 room
                 </div>
+                {openOptions && <GuestOptionsList options={options}/> }
             <span className="seperator"></span>
                 
             </div>
@@ -42,3 +51,31 @@ return (
 }
 
 export default Header
+
+function GuestOptionsList({options}){
+    return (
+        <div className="guestOptions">
+    <OptionItem/>
+    <OptionItem/>
+    <OptionItem/>
+        </div>
+    )
+}
+
+function OptionItem(){
+    return(
+        <div className="guestOptionItem">
+        <span className="optionText">Adult</span>
+        <div className="optionCounter">
+            <button className="optionCounterBtn">
+                <HiMinus className="icon"/>
+            </button>
+            <span className="optionCounterNumber">2</span>
+            <button className="optionCounterBtn">
+                <HiPlus className="icon"/>
+            </button>
+        </div>
+    </div>
+
+    )
+}
